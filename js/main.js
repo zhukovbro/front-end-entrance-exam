823b35fb316d0937a3986f16108404b7a4fdd67f
zhukovbro <azhykov@gmail.com>
2024-08-02 15:27:02 +0200

    Верстка половины шаблона

--- js/main.js
+++ js/main.js
@@ -1,24 +1,173 @@
 import '../css/style.css'
-import javascriptLogo from '../javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import '../css/media.css'
+import avatar from '/avatar.svg'
+
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="container">
+    <div class="row-one">
+      <div class="row-one__left">
+        <img src="${avatar}" class="row-one__left__avatar" alt="">
+        <div class="row-one__left__info block-grey-radius">
+          <div class="row-one__left__info__line-top">
+            Hello 👋🏻 I’m
+          </div>
+          <div class="row-one__left__info__line-bottom">
+            <div class="row-one__left__info__line-bottom__name">
+              Karthik SR
+            </div>
+            <div class="row-one__left__info__line-bottom__job">
+              UX/UI Designer
+            </div>
+          </div>
+        </div>
+      </div> 
+      <div class="row-one__right block-grey-radius">
+        <div class="row-one__right__top">
+          Languages
+        </div>
+        <div class="row-one__right__bottom">
+          <div class="row-one__right__bottom__text">
+            <div class="row-one__right__bottom__text__row">
+              English
+            </div>
+            <div class="row-one__right__bottom__text__row">
+              Malayalam
+            </div>
+            <div class="row-one__right__bottom__text__row">
+              Hindi
+            </div>
+          </div>
+          <div class="row-one__right__bottom__lines">
+            <div class="row-one__right__bottom__lines__item"></div>
+            <div class="row-one__right__bottom__lines__item"></div>
+            <div class="row-one__right__bottom__lines__item"></div>
+          </div>
+        </div>
+      </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div class="row-two">
+      <div class="row-two__left block-grey-radius">
+        <div class="row-two__left__title">
+          Experience
+        </div>
+        <div class="row-two__left__experience">
+          <div class="row-two__left__experience__item">
+            <div class="row-one__right__bottom__lines__item__top">
+              <div class="row-two__left__experience__item__top">
+                Jun. 2023 - Present
+              </div>
+              <div class="row-two__left__experience__item__top__tag">
+                
+              </div>
+            </div>
+            <div class="row-two__left__experience__item__bottom">
+              <div class="row-two__left__experience__item__bottom__job">
+                <div class="row-two__left__experience__item__bottom__job__title">
+                  Marketing Manager
+                </div>
+                <div class="row-two__left__experience__item__bottom__job__sub-title">
+                  <div class="row-two__left__experience__item__bottom__job__sub-title__item">
+                    Pankayam
+                  </div>
+                  <div class="row-two__left__experience__item__bottom__job__sub-title__item">
+                    Full-time
+                  </div>
+                </div>
+              </div>
+              <div class="row-two__left__experience__item__bottom__description">
+                <ul>
+                  <li>Development of internal projects from scratch, product design of brands </li>
+                  <li>Landing page, webapps and hybrid apps</li>
+                  <li>Coordinating with outside agencies, art services, web designer, marketing, printers, and colleagues as necessary.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="row-two__left__experience__item">
+            <div class="row-two__left__experience__item__top">
+              2017 - Present
+            </div>
+            <div class="row-two__left__experience__item__bottom">
+              <div class="row-two__left__experience__item__bottom__job">
+                <div class="row-two__left__experience__item__bottom__job__title">
+                  Graphic / Web designer
+                </div>
+                <div class="row-two__left__experience__item__bottom__job__sub-title">
+                  <div class="row-two__left__experience__item__bottom__job__sub-title__item">
+                    Freelance
+                  </div>
+                </div>
+              </div>
+              <div class="row-two__left__experience__item__bottom__description">
+                <ul>
+                  <li>Development of internal projects from scratch, product design of brands </li>
+                  <li>Landing page, webapps and hybrid apps</li>
+                  <li>Coordinating with outside agencies, art services, web designer, marketing, printers, and colleagues as necessary.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="row-two__left__experience__item">
+            <div class="row-two__left__experience__item__top">
+              Sep. 2021 - Jun. 2023
+            </div>
+            <div class="row-two__left__experience__item__bottom">
+              <div class="row-two__left__experience__item__bottom__job">
+                <div class="row-two__left__experience__item__bottom__job__title">
+                  Legal Assistant
+                </div>
+                <div class="row-two__left__experience__item__bottom__job__sub-title">
+                  <div class="row-two__left__experience__item__bottom__job__sub-title__item">
+                    Law Firm
+                  </div>
+                  <div class="row-two__left__experience__item__bottom__job__sub-title__item">
+                    Intern
+                  </div>
+                </div>
+              </div>
+              <div class="row-two__left__experience__item__bottom__description">
+                <ul>
+                  <li>Provide administrative support to lawyer and enhance office effectiveness</li>
+                  <li>Handle communication with clients, witnesses etc.</li>
+                  <li>repare case briefs and summarize depositions, interrogatories and testimony</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row-two__right block-grey-radius">
+        <div class="row-two__right__title">
+          Tools
+        </div>
+        <div class="row-two__right__inner">
+          
+        </div>
+      </div>
+    </div>
+    <div class="row-three">
+      <div class="row-three__left block-grey-radius">
+        asdf
+      </div>
+      <div class="row-three__right">
+        <div class="row-three__right__top block-grey-radius">
+          asdf
+        </div>
+        <div class="row-three__right_bottom">
+          asdf
+        </div>
+      </div>
+    </div>
+    
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+
+
+
+
+
+
+
+
